837ab60908b0b82becc27768b3add3098e15ee03
Cristian Usuga <usugahiguitacristian@gmail.com>
2024-10-27 05:25:24 -0500

    modificaciones en report-service.properties para que conecte con rabbitmq, cambio de puerto al 15672 y cambio del nombre del host

--- user-service/src/main/java/com/springbootmicroservices/userservice/config/KeycloakConfig.java
+++ user-service/src/main/java/com/springbootmicroservices/userservice/config/KeycloakConfig.java
@@ -13,7 +13,7 @@ public class KeycloakConfig {
     public final static String serverUrl = "http://localhost:8181";
     public final static String realm = "master";
     public final static String clientId = "spring-boot-microservice-keycloak";
-    public final static String clientSecret = "hLfIzFvPpYhKAqmmM5tfqkrnSHEJdpAu";
+    public final static String clientSecret = "3sWadMtVUUOF3NsJcEbP1bHElTCGCmsr";
     final static String userName = "admin";
     final static String password = "admin";
 
